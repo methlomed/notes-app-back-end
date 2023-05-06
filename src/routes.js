53d52eebb123c46191d4de05526b6a60361d457c
methlomed <methlomed@gmail.com>
2023-05-06 23:15:23 +0700

    add method DELETE & funct deleteNoteByIdHandler

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const {
   getAllNoteshandler,
   getNoteByIdHandler,
   editNoteByIdHandler,
+  deleteNoteByIdHandler,
 } = require("./handler");
 
 const routes = [
@@ -25,6 +26,11 @@ const routes = [
     method: "GET",
     path: "/notes/{id}",
     handler: getNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
   },
 ];
 
